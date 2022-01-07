790682f989cd96eb2bff5e62c7a429eaf81862f1
Valentin Rentschler <valentin.rentschler@flowable.com>
2022-01-07 15:46:01 +0100

    module2

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import QuizQuestion from './QuizQuestion'
 
 let quizData = require('./quiz_data.json')
 
@@ -10,9 +11,8 @@ class Quiz extends Component {
     }
 
     render() {
-        return <div>Quiz<div className="QuizQuestion">        
-            {quizData.quiz_questions[0].instruction_text}
-        </div></div>
+        const { quiz_position } = this.state;
+        return <div><QuizQuestion quiz_question={quizData.quiz_questions[quiz_position - 1]}/></div>
     }
 
 }
