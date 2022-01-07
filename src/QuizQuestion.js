5fdfb48ae6e33a3761332201e4a81ef6ed347cf8
Valentin Rentschler <valentin.rentschler@flowable.com>
2022-01-07 16:43:29 +0100

    module6

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -3,13 +3,21 @@ import QuizQuestionButton from "./QuizQuestionButton"
 
 class QuizQuestion extends Component {
 
+    constructor(props) {
+        super(props)    
+        this.state = {incorrectAnswer: false}
+    }
+
     handleClick(buttonText) {    
         if (this.props.quiz_question.answer === buttonText) {
             this.props.showNextQuestionHandler()
+            this.setState({incorrectAnswer: false})
+        } else {
+            this.setState({incorrectAnswer: true})
         }
     }
 
-    render() {
+    render() {    
         return <main>
             <section>
                 <p>{this.props.quiz_question.instruction_text}</p>            
@@ -23,6 +31,7 @@ class QuizQuestion extends Component {
                     }                                               
                 </ul>
             </section>
+            {this.state.incorrectAnswer ? <p className="error">Sorry, that's not right</p> : null}
         </main>
     }
 
