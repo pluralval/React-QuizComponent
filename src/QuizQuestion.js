5b2f9d8351f549ba7eee85e583b968f923cdab6a
Valentin Rentschler <valentin.rentschler@flowable.com>
2022-01-07 16:37:55 +0100

    module5

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -3,14 +3,24 @@ import QuizQuestionButton from "./QuizQuestionButton"
 
 class QuizQuestion extends Component {
 
+    handleClick(buttonText) {    
+        if (this.props.quiz_question.answer === buttonText) {
+            this.props.showNextQuestionHandler()
+        }
+    }
+
     render() {
         return <main>
             <section>
                 <p>{this.props.quiz_question.instruction_text}</p>            
             </section>
             <section className="buttons">
-                <ul>            
-                    <QuizQuestionButton button_text={this.props.quiz_question.answer_options[0]}/>        
+                <ul>
+                    {
+                        this.props.quiz_question.answer_options.map((answer_option, index) => {                          
+                            return <QuizQuestionButton button_text={answer_option} key={index} clickHandler={this.handleClick.bind(this)}/>
+                        })
+                    }                                               
                 </ul>
             </section>
         </main>
